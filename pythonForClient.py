41afd67d32200c441238e3aaa819cfc81e848777
Vijaya Bhargavi Akavarapu <61415847+vijaya-bhargavi@users.noreply.github.com>
2020-03-17 21:56:10 +0530

    Update pythonForClient.py

--- pythonForClient.py
+++ pythonForClient.py
@@ -1,8 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
+
 img=cv2.imread("/home/vijaya/Desktop/client_result.png",-1)
 cv2.imshow("Recombined Image",img)
-#plt.subplot(111),plt.imshow(img,cmap="gray"),plt.title("Recombined Image from Client")
-#plt.xticks([]), plt.yticks([])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
