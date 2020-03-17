78ed40f44d820dd0f46c594336582ea93870aea5
vijaya-bhargavi <asv.bhargavi@gmail.com>
2020-03-17 22:05:37 +0530

    Python code to display client output

--- pythonForClient.py
+++ pythonForClient.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 img=cv2.imread("/home/vijaya/Desktop/client_result.png",-1)
+
 cv2.imshow("Recombined Image",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
