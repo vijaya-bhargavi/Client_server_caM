420cfd80764d2e824b54dbc946658c321c54b90f
vijaya-bhargavi <asv.bhargavi@gmail.com>
2020-03-17 21:50:43 +0530

    Python code to display channels of server output

---
+++ pythonForServer.py
@@ -0,0 +1,16 @@
+import cv2
+import matplotlib.pyplot as plt
+imgr=cv2.imread("/home/vijaya/Desktop/red.png")
+imgg=cv2.imread("/home/vijaya/Desktop/green.png")
+imgb=cv2.imread("/home/vijaya/Desktop/blue.png")
+
+plt.subplot(131),plt.imshow(imgr,cmap="gray"),plt.title("Red")
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(132),plt.imshow(imgg,cmap="gray"),plt.title("Green")
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(133),plt.imshow(imgb,cmap="gray"),plt.title("Blue")
+plt.xticks([]), plt.yticks([])
+
+plt.show()
