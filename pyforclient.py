fac13b0f8518a04dfe93cf82e2168d7fcd874df2
vijaya-bhargavi <asv.bhargavi@gmail.com>
2020-03-21 17:03:54 +0530

    java calls

--- pyforclient.py
+++ pyforclient.py
@@ -1,19 +1,10 @@
 import sys
-
 import cv2
-
 import matplotlib.pyplot as plt
 
 S=sys.argv
-
 img=cv2.imread(S[1],-1)
-
 cv2.imshow("Recombined Image",img)
 
-#plt.subplot(111),plt.imshow(img,cmap="gray"),plt.title("Recombined Image from Client")
-
-#plt.xticks([]), plt.yticks([])
-
 cv2.waitKey(0)
-
 cv2.destroyAllWindows()
